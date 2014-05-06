870da8baaa62ab3f2d7c5e57d329fe8e34c98b30
Eduard Florinescu <georgeflorinescu@gmail.com>
2014-05-06 13:20:58 +0300

    Removed hline

--- examples/test_aestheticmappings.py
+++ examples/test_aestheticmappings.py
@@ -14,9 +14,9 @@ YMAX = 0
 spowd = pd.read_csv(INPUT_FILE)
 
 for i in range(0,len(spowd)):
-    if (spowd["Watts"][i]>YMAX): 
+    if (spowd["Watts"][i]>YMAX):
         YMAX = spowd["Watts"][i]
-    if (spowd["Watts"][i]<YMIN): 
+    if (spowd["Watts"][i]<YMIN):
         YMIN = spowd["Watts"][i]
     COUNT+=1
     SSUM+=spowd["Watts"][i]
@@ -30,12 +30,9 @@ LOW_MARGIN = SSAVERAGE - SSAVERAGE * MARGIN
 pprint.pprint(spowd)
 #works with color="category" but not with color="Volts"
 print ggplot(aes(x='TIME', y='Watts', shape="category", color="Volts", ymin=YMIN, ymax=YMAX), data=spowd) + \
-    geom_hline(y=HIGH_MARGIN,color="blue", label = "Average 1%") + \
-    geom_hline(y=LOW_MARGIN,color="green", label = "Average -1%") + \
-    geom_hline(y=SSAVERAGE,color="red", label = "Average") + \
     geom_point() + \
     ggtitle(INPUT_FILE.split(".")[0]) + \
     xlab("Time") + \
-    ylab("Watts") 
+    ylab("Watts")
 
 plt.show(1)
